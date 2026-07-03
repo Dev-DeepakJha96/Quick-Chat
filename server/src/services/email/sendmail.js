046db6130bad10f24dcd068eff0a223508af1044
Deepak Jha <deepakjha3926@gmail.com>
2026-07-03 21:25:45 +0530

    M5: Make TLS rejectUnauthorized conditional on environment
    Previously always false, disabling TLS cert validation in all
    envs including production (MITM risk). Now production=true
    (validate) and dev=false (convenience for self-signed certs).
    
    Changes:
    - server/src/services/email/sendmail.js:14

--- server/src/services/email/sendmail.js
+++ server/src/services/email/sendmail.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
     pass: env.email.emailPass,
   },
   tls: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: env.isProduction,
   },
 });
 
